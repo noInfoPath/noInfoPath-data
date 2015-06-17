43cf775fecb8f332ced7329e16e9d45bfc3a4df6
Adarian Eastman <adarian@imginconline.com>
2015-06-17 15:34:19 -0400

    Fixed gruntfile for Jenkins testing purposes

--- gruntfile.js
+++ gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
           continuous: {
             configFile: 'karma.conf.js',
             singleRun: true,
-            browsers: ['PhantomJS']
+            browsers: ['Chrome']
           }
         },
         bumpup: {
