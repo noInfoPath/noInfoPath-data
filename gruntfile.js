6810e3a209bffaa4ca642a2ec2da81ae37f597ce
Ryan <ryan@imginconline.com>
2015-06-17 16:00:33 -0400

    Changed firefox back to chrome -_-

--- gruntfile.js
+++ gruntfile.js
@@ -34,7 +34,7 @@ module.exports = function(grunt) {
           continuous: {
             configFile: 'karma.conf.js',
             singleRun: true,
-            browsers: ['Firefox']
+            browsers: ['Chrome']
           }
         },
         bumpup: {
