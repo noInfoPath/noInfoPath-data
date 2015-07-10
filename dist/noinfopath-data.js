998ccb2e6cd6de34ce90bf0954a4dc8761c92a80
gochinj <jeff@imginconline.com>
2015-07-09 21:36:34 -0400

    Unstable: do not use.

--- dist/noinfopath-data.js
+++ dist/noinfopath-data.js
@@ -2,7 +2,7 @@
 
 /*
  *	# noinfopath-data
- *	@version 0.2.3
+ *	@version 0.2.4
  *
  *	## Overview
  *	NoInfoPath data provides several services to access data from local storage or remote XHR or WebSocket data services.
