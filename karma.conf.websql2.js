493b6c22e1de38635ecfe1f4c8797c2e0601440c
gochinj <jeff@imginconline.com>
2015-12-22 19:58:43 -0500

    Added dev dependency to websql-db.mock.js.

--- karma.conf.websql2.js
+++ karma.conf.websql2.js
@@ -20,6 +20,8 @@ module.exports = function(config) {
         'node_modules/angular/angular.js',
         'node_modules/angular-mocks/angular-mocks.js',
         'bower_components/ng-lodash/build/ng-lodash.js',
+        'bower_components/sql-parser/browser/sql-parser.js',
+        'bower_components/mock-websql/websql.js',
         'node_modules/noinfopath/dist/noinfopath.js',
         'node_modules/noinfopath-helpers/src/noinfopath-helpers.js',
         'node_modules/noinfopath-helpers/src/noinfopath-filters.js',
