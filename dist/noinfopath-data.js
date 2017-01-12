efda206b2dde2c68919813158788f580b6db63fc
Jeffrey A. Gochin <jeff@imginconline.com>
2017-01-11 21:50:45 -0500

    Fixed bug with noParameterParser.update. Sometimes, the dest object does not have a $setPristine method.

--- dist/noinfopath-data.js
+++ dist/noinfopath-data.js
@@ -1,7 +1,7 @@
 //globals.js
 /*
  *	# noinfopath-data
- *	@version 2.0.31
+ *	@version 2.0.32
  *
  *	## Overview
  *	NoInfoPath data provides several services to access data from local storage or remote XHR or WebSocket data services.
@@ -7719,7 +7719,7 @@ var GloboTest = {};
 					}
 				});
 
-				dest.$setPristine();
+				if(dest.$setPristine) dest.$setPristine();
 			};
 
 			this.updateOne = function(ctrl, value) {
--- src/parameter-parser.js
+++ src/parameter-parser.js
@@ -39,7 +39,7 @@
 					}
 				});
 
-				dest.$setPristine();
+				if(dest.$setPristine) dest.$setPristine();
 			};
 
 			this.updateOne = function(ctrl, value) {
