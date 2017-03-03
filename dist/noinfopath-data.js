87662e9c2504104f9f1f6a0f161f7f30c8fdcb20
Adarian Eastman <adarian@imginconline.com>
2017-03-03 11:09:38 -0500

    Built 1.2.31

--- dist/noinfopath-data.js
+++ dist/noinfopath-data.js
@@ -1,7 +1,7 @@
 //globals.js
 /*
  *	# noinfopath-data
- *	@version 1.2.30
+ *	@version 1.2.31
  *
  *	## Overview
  *	NoInfoPath data provides several services to access data from local storage or remote XHR or WebSocket data services.
