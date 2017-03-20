b65afb6ef4f6b0008e41990f26cb87dc43fdbce3
Jeffrey A. Gochin <jeff@imginconline.com>
2017-03-20 13:33:52 -0400

    Bumped version to 2.0.46

--- dist/noinfopath-data.js
+++ dist/noinfopath-data.js
@@ -5,7 +5,7 @@
 	*	NoInfoPath Data (noinfopath-data)
 	*	=============================================
 	*
-	*	*@version 2.0.45* [![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
+	*	*@version 2.0.46* [![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 	*
 	*	Copyright (c) 2017 The NoInfoPath Group, LLC.
 	*
@@ -89,7 +89,7 @@ angular.module("noinfopath.data")
 	*
 	*	___
 	*
-	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 	*
 	*	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 	*
@@ -535,7 +535,7 @@ angular.module("noinfopath.data")
  *
  *	___
  *
- *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+ *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
  *
  *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
  *
@@ -2106,7 +2106,7 @@ angular.module("noinfopath.data")
 *
 *	___
 *
-*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 *
 *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 *
@@ -2243,7 +2243,7 @@ angular.module("noinfopath.data")
 *
 *	___
 *
-*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 *
 *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 *
@@ -2396,7 +2396,7 @@ angular.module("noinfopath.data")
  *
  *	___
  *
- *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+ *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
  *
  *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
  *
@@ -3446,7 +3446,7 @@ var GloboTest = {};
 	*
 	*	___
 	*
-	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 	*
 	*	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 	*
@@ -4898,7 +4898,7 @@ var GloboTest = {};
  *
  *	___
  *
- *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+ *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
  *
  *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
  *
@@ -5794,7 +5794,7 @@ var GloboTest = {};
  *
  *	___
  *
- *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+ *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
  *
  *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
  *
@@ -7654,7 +7654,7 @@ var GloboTest = {};
  *
  *	___
  *
- *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+ *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
  *
  *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
  *
@@ -8001,7 +8001,7 @@ var GloboTest = {};
 *
 *	___
 *
-*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 *
 *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 *
@@ -8227,7 +8227,7 @@ var GloboTest = {};
 	*
 	*	___
 	*
-	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 	*
 	*	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 	*
@@ -8366,7 +8366,7 @@ var GloboTest = {};
 *
 *	___
 *
-*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 *
 *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 *
@@ -8829,7 +8829,7 @@ var GloboTest = {};
 	*
 	*	___
 	*
-	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+	*	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 	*
 	*	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 	*
--- docs/classes.md
+++ docs/classes.md
@@ -2,7 +2,7 @@
 
 ___
 
-[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.45*
+[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.46*
 
 [![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
 
@@ -284,105 +284,3 @@ Creates and adds a new NoSortExpression into the underlying array that NoSort re
 |column|String|The name of the column filter on.|
 |dir|String|(Optional) One of the following values: `asc`, `desc`.|
 
-<<<<<<< HEAD
-=======
-### Class NoDataModel
-
- This class provides functionality to help other NoInfoPath services to
-access and utilitze data in a consistant way. It provides a pristine
-attribute to the data so a directive can 'roll back' a change, for example.
-
-#### Constructor
-
-var foo = new noInfoPath.data.NoDataModel(model);
-
-|Parameter|Type|Description|
-|*********|****|***********|
-|model|Object|Required. NoDataModel requires an data model being passed in due to this extending an existing data model.|
-
-#### Properties
-
-|Name|Type|Description|
-|----|----|-----------|
-|data|NoResults Object|Returns the data wrapped in a NoInfoPath NoResults object|
-|pristine|NoResults Object|Returns the pristine data wrapped in a NoInfoPath NoResults object|
-|__type|String|Returns the type of NoInfoPath object. In this case, it will return "NoDataModel"|
-
-##### data
-
-Returns an object that is saved within the NoDataModel.
-
-##### pristine
-
-Returns an object that is the pristine version of the data. This enables data rollbacks using the undo() method.
-
-##### __type
-
-Returns a string that explains that this is an object that was created by the NoDataModel class. Always returns "NoDataModel".
-
-
-#### Methods
-
-|Name|Description|
-|----|-----------|
- |clean()|Removes any Angular properties off the data object, and cleans up 'falsy' values to null|
-|undo()|Sets the data property back to what is stored in the pristine property|
-|update(data)|Updtes the data with a matching data object|
-
-##### clean()
-
-This method removes any Angular or Kendo data model properties off the data object. It also cleans up any
-falsy values and returns them as null.
-
-**Parameters**
-
-None
-
-**Returns**
-
-Undefined
-
-##### undo()
-
-This method returns the value contained within the NoDataModel back to the current pristine value.
-
-**Parameters**
-
-None
-
-##### update(data)
-
-This method updates the data contained within the data model to the data being passed in.
-
-**Parameters*
-
-|Name|Type|Description|
-|----|----|-----------|
-|data|Object|An object that will be saved within NoDataModel|
-
-data
-
-An object that is to be saved within the NoDataModel object. This data does not need to be flat.
-
-```js
-{
-	PersonID: "6a2bfe0f-29da-440d-e5b9-62262ac0345c",
-	PersonFirstName: "Foo",
-	PersonLastName: "Bar",
-	PersonAge: 25,
-	Mother: {
-		PersonID: "54dd9168-0111-43e3-9db8-77dc33169b41",
-		PersonFirstName: "Bridget",
-		PersonLastName: "Bar",
-		PersonAge: 50
-   }
- }
- ```
-
-**Returns**
-
-Undefined
-
-
-
->>>>>>> 635435863ef9848e630a832873b6ae677821bbac
