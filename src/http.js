4ff3b999c7f1ed8da8976cef30f88ab95c624147
Jeff <jeff@imginconline.com>
2017-05-04 23:49:55 -0400

    Added useQueryParams property to datasource config.

--- src/http.js
+++ src/http.js
@@ -4,7 +4,7 @@
  *
  *	___
  *
- *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.66*
+ *	[NoInfoPath Data (noinfopath-data)](home) *@version 2.0.67*
  *
  *	[![Build Status](http://gitlab.imginconline.com:8081/buildStatus/icon?job=noinfopath-data&build=6)](http://gitlab.imginconline.com/job/noinfopath-data/6/)
  *
@@ -311,7 +311,7 @@
 								},
 								withCredentials: true
 							};
-							req.params = _table.uri ? _resolveQueryParams(filters) : queryBuilder(filters, sort, page, select);
+							req.params = _table.uri || _table.useQueryParams ? _resolveQueryParams(filters) : queryBuilder(filters, sort, page, select);
 
 						$http(req)
 							.then(function (results) {
